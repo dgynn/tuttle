db4aaa06e4b510ae745e5f5ac75b77d43f70cdc3
Dave Gynn <davegynn@gmail.com>
2014-06-28 18:56:43 -0700

    add assets to precompile configuration

--- lib/tuttle/engine.rb
+++ lib/tuttle/engine.rb
@@ -1,5 +1,10 @@
 module Tuttle
   class Engine < ::Rails::Engine
     isolate_namespace Tuttle
+
+    initializer "tuttle.assets.precompile" do |app|
+      app.config.assets.precompile += %w(tuttle/application.css tuttle/application.js )
+    end
+
   end
 end
