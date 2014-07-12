06b7a29fadf5436993323cb750dd8001de4d8bcf
Dave Gynn <davegynn@gmail.com>
2014-07-11 18:36:05 -0700

    eager_load the application any time classes are reloaded

--- app/controllers/tuttle/application_controller.rb
+++ app/controllers/tuttle/application_controller.rb
@@ -1,5 +1,19 @@
 module Tuttle
   class ApplicationController < ActionController::Base
     abstract!
+
+    before_action :check_reload_status
+
+    def check_reload_status
+      if Tuttle::Engine.reload_needed && !Rails.configuration.eager_load
+        Rails.logger.warn('Tuttle: Eager-loading application')
+        # Rails::Application::Finisher defines an initializer that *would* execute
+        #   these two lines if eager_load were enabled
+        # ActiveSupport.run_load_hooks(:before_eager_load, Rails.application)
+        Rails.configuration.eager_load_namespaces.each(&:eager_load!)
+        Tuttle::Engine.reload_needed = false
+      end
+    end
+
   end
 end
--- lib/tuttle/engine.rb
+++ lib/tuttle/engine.rb
@@ -2,9 +2,18 @@ module Tuttle
   class Engine < ::Rails::Engine
     isolate_namespace Tuttle
 
+    attr_accessor :reload_needed
+
     initializer "tuttle.assets.precompile" do |app|
       app.config.assets.precompile += %w(tuttle/application.css tuttle/application.js )
     end
 
+    initializer :tuttle_track_reloads, group: :all do
+      ActionDispatch::Reloader.to_prepare do
+        Rails.logger.warn('Tuttle: ActionDispatch::Reloader called to_prepare')
+        Tuttle::Engine.reload_needed = true
+      end
+    end
   end
+
 end
