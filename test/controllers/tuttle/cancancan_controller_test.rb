0cd264bd32ee705b651ae58ab7a7c9914c8b0bab
Dave Gynn <davegynn@gmail.com>
2014-12-30 10:25:51 -0800

    add fixtures for users and test cases for cancancan rule tester

--- test/controllers/tuttle/cancancan_controller_test.rb
+++ test/controllers/tuttle/cancancan_controller_test.rb
@@ -21,9 +21,43 @@ module Tuttle
       assert_not_nil assigns(:action)
       assert_not_nil assigns(:cancan_user)
       assert_not_nil assigns(:ability)
+      assert_nil assigns(:subject)
     end
-  end
 
-  # TODO: test rule_tester with arguments
+    test 'should get rule_tester with subject' do
+      get :rule_tester, { :action_name => 'manage', :subject_class => 'User' }
+
+      assert_response :success
+
+      assert_not_nil assigns(:action)
+      assert_not_nil assigns(:cancan_user)
+      assert_not_nil assigns(:ability)
+      assert_not_nil assigns(:subject)
+    end
+
+    test 'should get rule_tester with subject and subject_id' do
+      get :rule_tester, { :action_name => 'manage', :subject_class => 'User', :subject_id => 1 }
+
+      assert_response :success
+
+      assert_not_nil assigns(:action)
+      assert_not_nil assigns(:cancan_user)
+      assert_not_nil assigns(:ability)
+      assert_not_nil assigns(:subject)
+    end
+
+    test 'should get rule_tester with invalid subject' do
+
+      get :rule_tester, { :action_name => 'manage', :subject_class => 'ClassDoesNotExist' }
+
+      assert_response :success
+
+      assert_not_nil assigns(:action)
+      assert_not_nil assigns(:cancan_user)
+      assert_not_nil assigns(:ability)
+      assert_nil assigns(:subject)
+    end
+
+  end
 
 end
