1952a6c74d14c3517076789165dbd786b661f253
Dave Gynn <davegynn@gmail.com>
2015-01-04 18:07:00 -0800

    require Tuttle::VERSION for older versions of ruby i do not see what changed that makes this load correctly in 2.1.5+

--- app/controllers/tuttle/home_controller.rb
+++ app/controllers/tuttle/home_controller.rb
@@ -1,5 +1,8 @@
 require_dependency 'tuttle/application_controller'
 
+# NOTE: needed for ruby < 2.1.5 - should figure out why
+require 'tuttle/version'
+
 module Tuttle
   class HomeController < ApplicationController
 
