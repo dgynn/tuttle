3cfccc3252faafc938c5ee6a6630038077e234ed
Dave Gynn <davegynn@gmail.com>
2015-01-05 22:50:44 -0800

    update boot.rb to work better with appraisals

--- Appraisals
+++ Appraisals
@@ -1,8 +1,7 @@
 appraise 'rails-4.1' do
-  gem 'rails', '~>4.1.4'
+  gem 'rails', '~>4.1'
 end
 
 appraise 'rails-4.2' do
-  gem 'rails', '~>4.2.0'
+  gem 'rails', '~>4.2'
 end
-
--- test/dummy/config/boot.rb
+++ test/dummy/config/boot.rb
@@ -2,9 +2,10 @@ require 'rubygems'
 gemfile = File.expand_path('../../../../Gemfile', __FILE__)
 
 if File.exist?(gemfile)
-  ENV['BUNDLE_GEMFILE'] = gemfile
-  require 'bundler'
-  Bundler.setup
+  ENV['BUNDLE_GEMFILE'] ||= gemfile
 end
+
+require 'bundler'
+Bundler.setup
 
 $LOAD_PATH.unshift File.expand_path('../../../../lib', __FILE__)
