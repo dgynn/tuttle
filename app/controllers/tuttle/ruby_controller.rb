1ff267d4e201a962f0f840bd8ea3f69f68313eed
Dave Gynn <davegynn@gmail.com>
2015-01-10 08:21:13 -0800

    filter some environment variable values

--- app/controllers/tuttle/ruby_controller.rb
+++ app/controllers/tuttle/ruby_controller.rb
@@ -4,6 +4,8 @@ module Tuttle
   class RubyController < ApplicationController
 
     def index
+      # TODO: need better filter for sensitive values. this covers DB-style URLs with passwords.
+      @filtered_env = ENV.to_hash.tap{ |h| h.each{ |k,_v| h[k] = '--FILTERED--' if /.*_URL$/ =~ k  } }
     end
 
   end
