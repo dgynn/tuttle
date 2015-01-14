8157363256b5a2416380c3e572358c6dcba847ea
Dave Gynn <davegynn@gmail.com>
2015-01-14 12:09:51 -0800

    add some more exclusions to ENV reporting

--- app/controllers/tuttle/ruby_controller.rb
+++ app/controllers/tuttle/ruby_controller.rb
@@ -4,8 +4,8 @@ module Tuttle
   class RubyController < ApplicationController
 
     def index
-      # TODO: need better filter for sensitive values. this covers DB-style URLs with passwords.
-      @filtered_env = ENV.to_hash.tap{ |h| h.each{ |k,_v| h[k] = '--FILTERED--' if /.*_URL$/ =~ k  } }
+      # TODO: need better filter for sensitive values. this covers DB-style URLs with passwords, passwords, and keys
+      @filtered_env = ENV.to_hash.tap{ |h| h.each{ |k,_v| h[k] = '--FILTERED--' if /.*_(URL|PASSWORD|KEY|KEY_BASE)$/ =~ k  } }
     end
 
   end
