dfea41221eeb501a2314f8d12d396e241b10e8c9
Dave Gynn <davegynn@gmail.com>
2015-05-30 12:12:12 -0700

    freeze some strings

--- app/helpers/tuttle/application_helper.rb
+++ app/helpers/tuttle/application_helper.rb
@@ -1,8 +1,8 @@
 module Tuttle
   module ApplicationHelper
 
-    def truth_label(is_true, true_label='true', false_label='false')
-      "<span class='label label-#{ is_true ? 'success':'danger'}'>#{ is_true ? true_label : false_label}</span>".html_safe
+    def truth_label(is_true, true_label='true'.freeze, false_label='false'.freeze)
+      "<span class='label label-#{ is_true ? 'success'.freeze : 'danger'.freeze}'>#{ is_true ? true_label : false_label}</span>".html_safe
     end
 
   end
