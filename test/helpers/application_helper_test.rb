70ca8228cba21f884830cf2391f2549ba14473eb
Dave Gynn <davegynn@gmail.com>
2015-06-21 20:10:00 -0700

    convert array to hash without .to_h which did not work in ruby 2.0

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -7,7 +7,7 @@ module Tuttle
 
     test 'tuttle_redacted should redact passwords' do
       test_hash = { 'password' => 'PASSWORD', 'key1' => 'VALUE1' }
-      test_results = tuttle_redacted(test_hash.each) { |key, value| [key, value] }.to_h
+      test_results = Hash[tuttle_redacted(test_hash.each) { |key, value| [key, value] }]
       assert_equal '--HIDDEN--', test_results['password']
       assert_equal test_hash['key1'], test_results['key1']
     end
