d93775e53206699024bfb6afd423961869f06059
Dave Gynn <davegynn@gmail.com>
2015-12-31 12:09:53 -0800

    mattr_accessor does not support defaults in active_support 4.0

--- lib/tuttle/engine.rb
+++ lib/tuttle/engine.rb
@@ -1,14 +1,10 @@
-require 'rails'
 require 'rails/engine'
 
 module Tuttle
   class Engine < ::Rails::Engine
     isolate_namespace Tuttle
 
-    mattr_accessor :reload_needed, :session_start
-    mattr_accessor :session_id do
-      SecureRandom.uuid
-    end
+    mattr_accessor :reload_needed, :session_start, :session_id
 
     mattr_reader :logger
 
@@ -30,6 +26,8 @@ module Tuttle
       next unless Tuttle.enabled
 
       Tuttle::Engine.session_start = Time.now
+      Tuttle::Engine.session_id = SecureRandom.uuid
+
       @@logger = ::Logger.new("#{Rails.root}/log/tuttle.log")
       Tuttle::Engine.logger.info('Tuttle engine started')
 
