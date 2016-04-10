45ffa9d11a9e80a1c4fe5d1433eac012c653ecfe
Dave Gynn <davegynn@gmail.com>
2016-04-09 19:34:52 -0700

    handle AMS refactoring of ADAPTER_MAP private constant

--- app/controllers/tuttle/active_model_serializers_controller.rb
+++ app/controllers/tuttle/active_model_serializers_controller.rb
@@ -10,6 +10,12 @@ module Tuttle
       @serializers = ::ActiveModel::Serializer.descendants
       @serializers.sort_by!(&:name)
 
+      @adapter_map = if ActiveModel::Serializer::Adapter.const_defined?(:ADAPTER_MAP)
+                       ActiveModel::Serializer::Adapter.const_get(:ADAPTER_MAP)
+                     else
+                       ActiveModelSerializers::Adapter.const_get(:ADAPTER_MAP)
+                     end
+
       unless defined?(ActiveModelSerializers)
         render 'index9'
       end
