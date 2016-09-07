d3d99601a1c700750852b77e1142b4521b4c88b2
Dave Gynn <davegynn@gmail.com>
2016-09-07 13:20:20 -0700

    resolve brakeman warning about constantization

--- app/controllers/tuttle/cancancan_controller.rb
+++ app/controllers/tuttle/cancancan_controller.rb
@@ -9,25 +9,22 @@ module Tuttle
     end
 
     def rule_tester
-      @models = ActiveRecord::Base.descendants
+      @models = ActiveRecord::Base.descendants.reject(&:abstract_class)
       @action = params[:action_name] || 'read'
-      @subject = nil
-
-      subject_class = params[:subject_class]
-      subject_id = params[:subject_id]
-
-      if !subject_class.blank? && Kernel.const_defined?(subject_class)
-        begin
-          subject_klass = Kernel.const_get(params[:subject_class])
-          @subject = subject_klass.find(subject_id) unless subject_id.blank?
-          @subject ||= subject_klass.new
-        rescue
-          # Could not load subject
-        end
-      end
+      @subject = find_subject(params[:subject_class], params[:subject_id])
       @cancan_user = current_user || User.new
       @ability = Ability.new(@cancan_user)
     end
 
+    private
+    def find_subject(subject_class, subject_id)
+      subject_klass = @models.detect { |x| x.name == subject_class } if subject_class.present?
+      if subject_klass
+        subject_klass.find_or_initialize_by(:id => subject_id)
+      else
+        nil
+      end
+    end
   end
+
 end
