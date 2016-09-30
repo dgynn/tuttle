10f9380b741ed7601e9876839adb85eb092ee51a
Dave Gynn <davegynn@gmail.com>
2016-09-29 23:14:36 -0700

    add busted+ support to profiler middleware

--- lib/tuttle/middleware/request_profiler.rb
+++ lib/tuttle/middleware/request_profiler.rb
@@ -18,6 +18,8 @@ module Tuttle
           profile_memory(env, query_string)
         when 'ruby-prof', 'cpu'
           profile_cpu(env, query_string)
+        when 'busted'
+          profile_busted(env, query_string)
         else
           @app.call(env)
         end
@@ -79,6 +81,105 @@ module Tuttle
         profiler.new(data).print(result, options)
 
         [200, { 'Content-Type' => content_type }, [result.string]]
+      end
+
+      def profile_busted(env, query_string)
+        # Note: Requires Busted (of course) and DTrace so will need much better error handling and information
+        # For DTrace on OS X (post 10.11) you may need to disable SIP as well as be running with root privileges
+        # https://derflounder.wordpress.com/2015/10/01/system-integrity-protection-adding-another-layer-to-apples-security-model/
+
+        require 'busted'
+        require 'busted/profiler/default' # Required so `autoload :Default` does not get included in trace
+
+        # Notes:
+        # Much of this is per https://tenderlovemaking.com/2015/12/23/inline-caching-in-mri.html
+        #   and https://github.com/charliesome/charlie.bz/blob/master/posts/things-that-clear-rubys-method-cache.md
+        # RubyVM.stat tracks the :global_method_state, :global_constant_state, and :class_serial
+        #   :global_method_state - "global serial number that increments every time certain classes get mutated"
+        #      ** Changes to this are BAD because they invalidate all caches
+        #   :global_constant_state - counter of constants defined *or redefined(?)*
+        #   :class_serial - global serial number that increments every time a method is Class is created/modified (?-verify)
+        # Since Ruby 2.1(2.2?) the method caches are per-Class rather than global.
+        #   So clearing the method cache of a single Class is less of a performance hit than blowing away the entire method cache
+        #
+        # Busted Dtraces :method-cache-clear internal events which are when Ruby says the method cache was cleared
+        # The @tracer Dtraces Class/Module definitions (:end) and calls to C method (:c_call) which would likely cause a method cache clear
+        #
+        # From the observed results...
+        #   :method-cache-clear may fire more times than RubyVM.stat[]
+        cache_busters = []
+        # These methods *may* cause the method cache to be invalidated
+        trace_methods = Set.new([:extend, :include, :const_set, :remove_const, :alias_method, :remove_method,
+                                 :prepend, :append_features, :prepend_features,
+                                 :public_constant, :private_constant, :autoload,
+                                 :define_method, :define_singleton_method])
+
+        # Trace class definitions (which always invalidate the cache) and c-calls which may invalidate the cache
+        # TODO: decide whether :start or :end calls should be traced
+        @tracer = TracePoint.new(:end, :c_call) do |trace|
+          if trace.event == :end
+            cache_busters << {
+                :event => trace.event,
+                :location => "#{trace.path}:#{trace.lineno}",
+                :target_class => trace.self
+            }
+          elsif trace_methods.include?(trace.method_id)
+            cache_busters << {
+                :event => trace.event,
+                :location => "#{trace.path}:#{trace.lineno}",
+                :target_class => trace.self.class,
+                :defined_class => trace.defined_class,
+                :method_id => trace.method_id
+            }
+          end
+        end
+
+        # Trace the request and capture the RubyVM.stat before/after
+        vmstat_before = RubyVM.stat
+        @tracer.enable
+        results = Busted.run(trace: true) do
+          _, _, body = @app.call(env)
+          body.close if body.respond_to?(:close)
+        end
+        @tracer.disable
+        vmstat_after = RubyVM.stat
+
+        # Prepare the output
+        # TODO: convert to ERB (with table?)
+        output = "\nRubyVM.stat:\n".dup
+        [:global_method_state, :global_constant_state, :class_serial].each do |stat|
+          output << sprintf("%-25s %10d %10d %+d\n", stat, vmstat_before[stat], vmstat_after[stat],  vmstat_after[stat] - vmstat_before[stat])
+        end
+
+        output << "\nTraces (method-cache-clear):\n"
+        results[:traces][:method].each do |trace|
+          output << "#{trace[:class]} - #{trace[:sourcefile]}##{trace[:lineno]}\n"
+        end
+
+        output << "\nTraces (method cache clearing calls):\n"
+        cache_busters.each do |trace_info|
+          if trace_info[:event] == :c_call
+            output << sprintf("%s\#%s: %s %s\n",
+                              trace_info[:defined_class],
+                              trace_info[:method_id],
+                              trace_info[:target_class],
+                              # trace_info[:target_object_id],
+                              trace_info[:location])
+          else
+            output << sprintf("Class/Module Definition: %s %s\n",
+                              trace_info[:target_class],
+                              #trace_info[:target_object_id],
+                              trace_info[:location])
+          end
+        end
+
+        [ 200,
+          { 'Content-Type' => 'text/plain' },
+          [ "Tuttle - Ruby Method/Constant Caches Request Observer v0.0.1\n",
+            "Ruby Version: #{RUBY_VERSION}\n",
+            output
+          ]
+        ]
       end
 
     end
