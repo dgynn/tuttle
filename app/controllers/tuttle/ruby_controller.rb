4cb3b8e294b73263f33ec3b131e82ceb57ef6050
Dave Gynn <davegynn@gmail.com>
2017-01-01 09:34:16 -0800

    add counts to ruby config tabs

--- app/controllers/tuttle/ruby_controller.rb
+++ app/controllers/tuttle/ruby_controller.rb
@@ -5,9 +5,13 @@ require_dependency 'tuttle/application_controller'
 module Tuttle
   class RubyController < ApplicationController
 
+    ENV_FILTERS = [/.*_(URL|PASSWORD|KEY|KEY_BASE|AUTHENTICATION)$/]
+
     def index
       # TODO: need better filter for sensitive values. this covers DB-style URLs with passwords, passwords, and keys
-      @filtered_env = ENV.to_hash.tap { |h| h.each { |k, _v| h[k] = '--FILTERED--' if /.*_(URL|PASSWORD|KEY|KEY_BASE|AUTHENTICATION)$/ =~ k } }
+      env_hash = ENV.to_hash
+      env_hash = ActionDispatch::Http::ParameterFilter.new(ENV_FILTERS).filter(env_hash) unless params[:nofilter]
+      @filtered_env = env_hash.sort
     end
 
     def tuning
