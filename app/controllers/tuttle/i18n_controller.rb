57a250c718cfa539df87a2d4a1eb19d0e5725c4d
Dave Gynn <davegynn@gmail.com>
2017-11-27 16:56:18 -0800

    display all localization format translations

--- app/controllers/tuttle/i18n_controller.rb
+++ app/controllers/tuttle/i18n_controller.rb
@@ -3,6 +3,8 @@ require_dependency 'tuttle/application_controller'
 module Tuttle
   class I18nController < ApplicationController
 
+    before_action :set_sorted_locales
+
     def index
     end
 
@@ -11,6 +13,13 @@ module Tuttle
 
     def translations
       @translations = I18n.backend.send(:translations)
+    end
+
+    private
+
+    def set_sorted_locales
+      @sorted_locales = [I18n.default_locale] +
+        (I18n.available_locales.map(&:to_s).sort.map(&:to_sym) - [I18n.default_locale])
     end
 
   end
